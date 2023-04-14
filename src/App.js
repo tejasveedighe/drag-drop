122af1bbedba865aab3ccad53ccf425c869211d1
tejasveedighe <tejasveedighe@gmail.com>
2023-04-14 11:39:14 +0530

    Xarrows
    Used Xarrows library to add connective arrows to the boxes

--- src/App.js
+++ src/App.js
@@ -1,34 +1,46 @@
 import React from "react";
 import Draggable from "react-draggable";
 import "./App.css";
+import Xarrow, { useXarrow, Xwrapper } from "react-xarrows";
 
-class App extends React.Component {
-	render() {
-		return (
-			<div className="parent">
-				<Draggable
-					axis="both"
-					handle=".handle"
-					defaultPosition={{ x: 0, y: 0 }}
-					position={null}
-					grid={[100, 25]}
-					scale={1}
-					onStart={this.handleStart}
-					onDrag={this.handleDrag}
-					onStop={this.handleStop}
-					// bounds={{ left: 0, top: 0, right: 500, bottom: 500 }}
-					bounds="parent"
-					defaultClassNameDragged="dragged"
-					defaultClassNameDragging="dragging"
-				>
-					<div className="App handle">
-						<div>Drag from here</div>
-						<div>This readme is really dragging on...</div>
-					</div>
-				</Draggable>
+const DraggableBox = ({ id }) => {
+	const updateArrow = useXarrow();
+	return (
+		<Draggable
+			axis="both"
+			handle=".handle"
+			defaultPosition={{ x: 0, y: 0 }}
+			position={null}
+			grid={[100, 25]}
+			scale={1}
+			onDrag={updateArrow}
+			onStop={updateArrow}
+			bounds="parent"
+			defaultClassNameDragged="dragged"
+			defaultClassNameDragging="dragging"
+		>
+			<div id={id} className="App handle">
+				<div>Box: {id}</div>
 			</div>
-		);
-	}
-}
+		</Draggable>
+	);
+};
+
+const App = () => {
+	return (
+		<div className="parent">
+			<Xwrapper>
+				<DraggableBox id={"elem1"} />
+				<DraggableBox id={"elem2"} />
+				<Xarrow
+					start={"elem1"}
+					end={"elem2"}
+					lineColor={"green"}
+					headColor={"green"}
+				/>
+			</Xwrapper>
+		</div>
+	);
+};
 
 export default App;
