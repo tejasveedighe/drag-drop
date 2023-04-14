ec11764a271884a19b6e67890ea5bd54e788efcb
tejasveedighe <tejasveedighe@gmail.com>
2023-04-14 10:24:57 +0530

    Added bounds of parent container width, need to fix the position of parent as the margin and padding does not make bound of the draggable component

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import Draggable from "react-draggable";
 import "./App.css";
 
 class App extends React.Component {
 	render() {
 		return (
-			<Draggable
-				axis="both"
-				handle=".handle"
-				defaultPosition={{ x: 0, y: 0 }}
-				position={null}
-				grid={[100, 25]}
-				scale={1}
-				onStart={this.handleStart}
-				onDrag={this.handleDrag}
-				onStop={this.handleStop}
-				bounds={{ left: 0, top: 0, right: 500, bottom: 500 }}
-				defaultClassNameDragged="dragged"
-				defaultClassNameDragging="dragging"
-			>
-				<div className="App">
-					<div className="handle">Drag from here</div>
-					<div>This readme is really dragging on...</div>
-				</div>
-			</Draggable>
+			<div className="parent">
+				<Draggable
+					axis="both"
+					handle=".handle"
+					defaultPosition={{ x: 0, y: 0 }}
+					position={null}
+					grid={[100, 25]}
+					scale={1}
+					onStart={this.handleStart}
+					onDrag={this.handleDrag}
+					onStop={this.handleStop}
+					// bounds={{ left: 0, top: 0, right: 500, bottom: 500 }}
+					bounds="parent"
+					defaultClassNameDragged="dragged"
+					defaultClassNameDragging="dragging"
+				>
+					<div className="App handle">
+						<div>Drag from here</div>
+						<div>This readme is really dragging on...</div>
+					</div>
+				</Draggable>
+			</div>
 		);
 	}
 }
