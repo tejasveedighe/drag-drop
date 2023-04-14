b4cd5aa7b5c9635fc280a2341136a2ea333a0081
tejasvee-appsdeployer <tejasvee@appsdeployer.com>
2023-04-14 09:54:07 +0530

    Added a basic drag object

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Draggable from "react-draggable";
 import "./App.css";
-import Builder from "./components/Builder/Builder";
 
-function App() {
-	return (
-		<div className="App">
-			<Builder />
-		</div>
-	);
+class App extends React.Component {
+	render() {
+		return (
+			<Draggable
+				axis="both"
+				handle=".handle"
+				defaultPosition={{ x: 0, y: 0 }}
+				position={null}
+				grid={[25, 25]}
+				scale={1}
+				onStart={this.handleStart}
+				onDrag={this.handleDrag}
+				onStop={this.handleStop}
+			>
+				<div className="App">
+					<div className="handle">Drag from here</div>
+					<div>This readme is really dragging on...</div>
+				</div>
+			</Draggable>
+		);
+	}
 }
 
 export default App;
